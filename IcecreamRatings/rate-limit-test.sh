8fb00c695eaaeb817ac92a63e0a3d700d2c09c90
snobu <foo@snobu.org>
2022-01-19 15:42:30 +0200

    Fix rate limit script

--- IcecreamRatings/rate-limit-test.sh
+++ IcecreamRatings/rate-limit-test.sh
@@ -2,13 +2,19 @@
 
 URL=https://team9apim.azure-api.net/api/GetProducts/api/GetProducts
 
-[ $APIMKEY ] || echo "Export your API Management key first: 'export APIMKEY=value'" && exit 1
+if [ -z $APIMKEY ]
+then
+    echo "Export your API Management key first: 'export APIMKEY=value"
+    exit 1
+else
+    echo "APIMKEY is $APIMKEY"
+fi
 
 for i in `seq 10`
 do
     for i in `seq 10`
     do
-        curl -si -o /dev/null -w "%{http_code}\n" $URL \
+        curl -si -o /dev/null -w "%{url} | HTTP response code: %{http_code}\n" $URL \
             -H "Ocp-Apim-Subscription-Key: $APIMKEY" &
     done
 done
