3553eba35fd08c55a25ff9220bcc3c6467232730
Richard Astbury <richard.astbury@gmail.com>
2022-01-20 09:43:46 +0000

    add http api call

--- batchfiles/BatchEntity.cs
+++ batchfiles/BatchEntity.cs
@@ -2,13 +2,28 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using System.Net.Http;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace Company.Function
 {
     [JsonObject(MemberSerialization.OptIn)]    
     public class BatchEntity 
     {
+
+        async Task<bool> CallApi(RequestModel model)
+        {
+            const string url = "https://serverlessohmanagementapi.trafficmanager.net/api/order/combineOrderContent";
+
+            using var client = new HttpClient();
+            
+            var result = await client.PostAsync(url, new StringContent(JsonConvert.SerializeObject(model), System.Text.Encoding.UTF8, "appliction/json"));
+            return result.IsSuccessStatusCode;
+        }
+
+
+
         [JsonProperty("files")]
         public List<string> Files { get; set; } = new List<string>();
         public void AddFile(string filename)
@@ -16,7 +31,11 @@ namespace Company.Function
             Files.Add(filename);
             if (Files.Count == 3)
             {
-                // Todo: notify Serverless OpenHack API System
+                CallApi(new RequestModel{
+                    OrderHeaderDetailsCSVUrl = Files.FirstOrDefault(x => x.Contains("OrderHeaderDetails")),
+                    OrderLineItemsCSVUrl = Files.FirstOrDefault(x => x.Contains("OrderLineItems")),
+                    ProductInformationCSVUrl = Files.FirstOrDefault(x => x.Contains("ProductInformation"))
+                }).Wait();
             }
         }
 
---
+++ batchfiles/RequestModel.cs
@@ -0,0 +1,10 @@
+namespace Company.Function
+{
+  public class RequestModel
+  {
+    public string OrderHeaderDetailsCSVUrl { get; set; }
+    public string OrderLineItemsCSVUrl { get; set; }
+    public string ProductInformationCSVUrl { get; set; }
+  }
+
+}
